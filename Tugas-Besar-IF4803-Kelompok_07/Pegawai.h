c67ff2d01685496204f848566f83e1a1bc9bb809
RapaAhmad <rafaahmadaulia002@gmail.com>
2025-11-29 22:53:02 +0700

    Menambahkan adt untuk parent(divisi)

--- Tugas-Besar-IF4803-Kelompok_07/Pegawai.h
+++ Tugas-Besar-IF4803-Kelompok_07/Pegawai.h
@@ -1,6 +1,20 @@
 #ifndef PEGAWAI_H_INCLUDED
 #define PEGAWAI_H_INCLUDED
 
+struct Pegawai{
+    string namaPegawai;
+    string jabatanPegawai;
+    string genderPegawai;
+    int umurPegawai;
+    string NIK;
+};
 
+typedef struct elmC *addresC;
+
+struct elmC{
+    Pegawai infoC;
+    addresC next;
+    addresC prev;
+};
 
 #endif // PEGAWAI_H_INCLUDED
